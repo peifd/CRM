9e29b825584451d8395798525b474f5f952ccef9
peifd <peifandong@163.com>
2023-07-05 22:42:40 +0800

    增加随机数生成，用来做卡号

--- CRM/frmAddUser.cs
+++ CRM/frmAddUser.cs
@@ -146,6 +146,8 @@ namespace CRM
                     //    messagebox.show("此卡已经使用，不能重复使用");
                     //    return;
                     //}
+                    TimeSpan ts  = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+                    double times = ts.TotalSeconds;
                     sql = "Insert into User(ID,VipCardId,UserName,Sex,Tel,CreateTime,Money,Active,Address,Remark,UserType)values(@ID,@VipCardId,@UserName,@Sex,@Tel,@CreateTime,@Moeny,@Active,@Address,@Remark,@UserType)";
                     prm = new string[] { "@ID", "@VipCardId", "@UserName", "@Sex", "@Tel", "@CreateTime", "@Moeny", "@Active", "@Address", "@Remark", "@UserType" };
                     obj = new object[] { Guid.NewGuid().ToString("N"), Guid.NewGuid().ToString("N"), txtUserName.Text, radioButtonMan.Checked ? 1 : 2, txtTel.Text, DateTime.Now, 0, radioButtonOK.Checked ? 1 : 2, txtAddress.Text, txtRemark.Text, o.ToString() };
@@ -170,6 +172,38 @@ namespace CRM
                 TracingHelper.Error(ex, typeof(frmAddUser));
             }
 
+        }
+
+        // n 为生成随机数个数
+        private int[] GenerateUniqueRandom(int minValue, int maxValue, int n)
+        {
+            //如果生成随机数个数大于指定范围的数字总数，则最多只生成该范围内数字总数个随机数
+            if (n > maxValue - minValue + 1)
+                n = maxValue - minValue + 1;
+
+            int maxIndex = maxValue - minValue + 2;// 索引数组上限
+            int[] indexArr = new int[maxIndex];
+            for (int i = 0; i < maxIndex; i++)
+            {
+                indexArr[i] = minValue - 1;
+                minValue++;
+            }
+
+            Random ran = new Random();
+            int[] randNum = new int[n];
+            int index;
+            for (int j = 0; j < n; j++)
+            {
+                index = ran.Next(1, maxIndex - 1);// 生成一个随机数作为索引
+
+                //根据索引从索引数组中取一个数保存到随机数数组
+                randNum[j] = indexArr[index];
+
+                // 用索引数组中最后一个数取代已被选作随机数的数
+                indexArr[index] = indexArr[maxIndex - 1];
+                maxIndex--; //索引上限减 1
+            }
+            return randNum;
         }
 
         private void frmAddUser_FormClosing(object sender, FormClosingEventArgs e)
